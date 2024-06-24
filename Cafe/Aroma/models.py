e92b1679380395f0c8d7f59b6ebfe652fed27984
Parsa Behmanesh <141092647+pathilda@users.noreply.github.com>
2024-06-25 00:56:16 +0330

    Basic Setups
    basic setups and two models created and migrated

--- Cafe/Aroma/models.py
+++ Cafe/Aroma/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Admins(models.Model):
+    Username = models.CharField(max_length=255, blank= False, unique=True)
+    Email = models.CharField(max_length=255, blank= False, unique=True)
+    Password = models.CharField(max_length=255, blank= False, unique=True)
+
+
+class Storage(models.Model):
+    ID = models.AutoField(unique=True, primary_key=True)
+    Name = models.CharField(max_length=255, blank= False, unique=True)
+    Amount = models.CharField(max_length=255, blank= False)
